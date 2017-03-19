9d32ce79f8c4d8861d53a51a1520b41166a85026
Lin <weihaolin2008@qq.com>
2017-03-19 13:31:36 +0800

    Spring Elements视图

---
+++ src/main/java/cn/stu/edu/lin/controller/UrlController.java
@@ -0,0 +1,18 @@
+package cn.stu.edu.lin.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UrlController {
+
+	@RequestMapping("/index")
+	public String toIndex(HttpServletRequest request, Model model) {
+
+		return "index";
+	}
+}
