32eb4ff2f39f20e6de5100e43a7e6dd3a38fb6b5
Lin <weihaolin2008@qq.com>
2017-04-22 00:48:39 +0800

    异常处理器增加参数为空信息区分

--- src/main/java/cn/stu/edu/lin/handler/RestBaseExceptionHandler.java
+++ src/main/java/cn/stu/edu/lin/handler/RestBaseExceptionHandler.java
@@ -1,8 +1,12 @@
 package cn.stu.edu.lin.handler;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -29,8 +33,26 @@ public class RestBaseExceptionHandler {
 
 			// 如果是注解校验
 		} else {
-			return Resp.createError(RespCode.PARAM_ERR, ex.getFieldError().getField(),
-					ex.getFieldError().getDefaultMessage());
+			// 处理为空的错误
+			/**
+			 * an object/field-specific code, a field-specific code, a plain
+			 * error code. @DefaultMessageCodesResolver.resolveMessageCodes
+			 **/
+			List<ObjectError> list = ex.getAllErrors();
+			if (list != null && list.size() != 0) {
+				for (ObjectError objectError : list) {
+					String code = objectError.getCode();
+					// 可以有多个校验注解，只要有为空的校验不通过就返回参数缺失，三个区别
+					if (!StringUtils.isBlank(code) && (code.startsWith("NotBlank") || code.startsWith("NotNull")
+							|| code.startsWith("NotEmpty"))) {
+						return Resp.createError(RespCode.PARAM_ERR, ex.getFieldError().getField(),
+								ex.getFieldError().getField());
+					}
+				}
+			}
+
+			// 处理非空的其它错误
+			return Resp.createError(RespCode.PARAM_ERR, ex.getFieldError().getField(), ex.getFieldError().getField());
 		}
 
 	}
